8bb98fc270a72d3a865f159f3419798c7831c541
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 15:20:11 +0900

    add new activity

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/klaster/KlasterActivity.kt
@@ -0,0 +1,13 @@
+package jp.cordea.recyclerviewlibrarycomparison.klaster
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.cordea.recyclerviewlibrarycomparison.R
+
+class KlasterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_klaster)
+    }
+}
