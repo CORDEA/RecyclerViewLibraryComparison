c96b5dc269540b65f29004032d4947a47a19e1f2
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 13:43:38 +0900

    display items

--- app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/groupie/GroupieActivity.kt
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/groupie/GroupieActivity.kt
@@ -2,12 +2,41 @@ package jp.cordea.recyclerviewlibrarycomparison.groupie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import dagger.android.AndroidInjection
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import jp.cordea.recyclerviewlibrarycomparison.MainRepository
 import jp.cordea.recyclerviewlibrarycomparison.R
+import jp.cordea.recyclerviewlibrarycomparison.databinding.ActivityGroupieBinding
+import javax.inject.Inject
 
 class GroupieActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var adapter: GroupieAdapter
+
+    @Inject
+    lateinit var repository: MainRepository
+
+    private var disposable: Disposable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_groupie)
+        val binding = DataBindingUtil.setContentView<ActivityGroupieBinding>(this, R.layout.activity_groupie)
+        binding.recyclerView.adapter = adapter
+
+        disposable = repository.getLanguages()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ langs ->
+                adapter.update(langs.map { GroupieItemModel.from(it) })
+            }, {
+            })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }
