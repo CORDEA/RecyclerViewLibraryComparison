8d952cc83905876e6245004a54e02ddd933d0e9a
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 17:12:59 +0900

    add layout for epoxy

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/package-info.java
@@ -0,0 +1,6 @@
+@PackageModelViewConfig(rClass = R.class)
+@EpoxyDataBindingLayouts(R.layout.list_item_epoxy)
+package jp.cordea.recyclerviewlibrarycomparison;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
+import com.airbnb.epoxy.PackageModelViewConfig;
