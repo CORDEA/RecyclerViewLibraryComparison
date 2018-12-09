546eb3245b0b6bccd56a7cf3eb9fa332ad41abc8
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 15:40:01 +0900

    add adapter

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/klaster/KlasterAdapterBinder.kt
@@ -0,0 +1,41 @@
+package jp.cordea.recyclerviewlibrarycomparison.klaster
+
+import android.view.LayoutInflater
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import com.github.rongi.klaster.Klaster
+import jp.cordea.recyclerviewlibrarycomparison.Language
+import jp.cordea.recyclerviewlibrarycomparison.databinding.ListItemKlasterBinding
+import javax.inject.Inject
+
+class KlasterItemModel(
+    val title: String,
+    val description: String,
+    url: String
+) {
+    companion object {
+        fun from(language: Language) =
+            KlasterItemModel(language.name, language.designers.joinToString(", "), language.url)
+    }
+
+    val uri = url.toUri()
+}
+
+class KlasterAdapterBinder @Inject constructor(
+    private val layoutInflater: LayoutInflater
+) {
+    private var models: List<KlasterItemModel> = emptyList()
+
+    val adapter = Klaster.withViewHolder<ViewHolder>()
+        .itemCount(models.size)
+        .viewHolder { _, parent -> ViewHolder(ListItemKlasterBinding.inflate(layoutInflater, parent, false)) }
+        .bind { position -> binding.model = models[position] }
+        .build()
+
+    fun update(models: List<KlasterItemModel>) {
+        this.models = models
+        adapter.notifyDataSetChanged()
+    }
+
+    private class ViewHolder(val binding: ListItemKlasterBinding) : RecyclerView.ViewHolder(binding.root)
+}
