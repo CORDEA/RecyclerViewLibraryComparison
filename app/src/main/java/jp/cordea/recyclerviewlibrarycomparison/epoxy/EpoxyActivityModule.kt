ab9ddd3896913f16964c49dd04d5b31994331083
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 18:14:16 +0900

    bind activity

--- app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/epoxy/EpoxyActivityModule.kt
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/epoxy/EpoxyActivityModule.kt
@@ -1,10 +1,22 @@
 package jp.cordea.recyclerviewlibrarycomparison.epoxy
 
+import android.app.Activity
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface EpoxyActivityModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(
+        modules = [
+            EpoxyActivityBindModule::class
+        ]
+    )
     fun contributeEpoxyActivity(): EpoxyActivity
+}
+
+@Module
+interface EpoxyActivityBindModule {
+    @Binds
+    fun bindActivity(activity: EpoxyActivity): Activity
 }
