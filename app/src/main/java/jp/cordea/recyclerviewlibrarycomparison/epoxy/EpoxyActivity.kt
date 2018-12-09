2b2643725fe215a3f7536ce80b00f0ad91621838
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 16:42:27 +0900

    add new activity

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/epoxy/EpoxyActivity.kt
@@ -0,0 +1,13 @@
+package jp.cordea.recyclerviewlibrarycomparison.epoxy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.cordea.recyclerviewlibrarycomparison.R
+
+class EpoxyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_epoxy)
+    }
+}
