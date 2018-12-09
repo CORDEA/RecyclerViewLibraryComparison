7a4e67b1d67e254699edff36c5d6682170dc69c6
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 12:25:28 +0900

    add activity

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/groupie/GroupieActivity.kt
@@ -0,0 +1,13 @@
+package jp.cordea.recyclerviewlibrarycomparison.groupie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.cordea.recyclerviewlibrarycomparison.R
+
+class GroupieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_groupie)
+    }
+}
