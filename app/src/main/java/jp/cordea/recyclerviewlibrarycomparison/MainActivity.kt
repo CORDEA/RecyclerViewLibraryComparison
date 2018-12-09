ddba6922f3e479e49e99c198388444ceda80d6ac
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-09 13:41:27 +0900

    navigate to the groupie activity

--- app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/MainActivity.kt
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/MainActivity.kt
@@ -1,12 +1,21 @@
 package jp.cordea.recyclerviewlibrarycomparison
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import dagger.android.AndroidInjection
+import jp.cordea.recyclerviewlibrarycomparison.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var navigator: MainNavigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.groupieButton.setOnClickListener { navigator.navigateToGroupie() }
     }
 }
