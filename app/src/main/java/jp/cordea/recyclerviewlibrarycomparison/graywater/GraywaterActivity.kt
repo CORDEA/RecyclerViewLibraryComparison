cd9987c13e38e98d8632c21daaa006ea1b383bb7
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-10 22:32:58 +0900

    add new activity

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/graywater/GraywaterActivity.kt
@@ -0,0 +1,13 @@
+package jp.cordea.recyclerviewlibrarycomparison.graywater
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.cordea.recyclerviewlibrarycomparison.R
+
+class GraywaterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graywater)
+    }
+}
