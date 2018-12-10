3b40372949a76282566f10dde08456722f1c9244
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-10 23:53:05 +0900

    display items

--- app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/graywater/GraywaterActivity.kt
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/graywater/GraywaterActivity.kt
@@ -2,12 +2,40 @@ package jp.cordea.recyclerviewlibrarycomparison.graywater
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import jp.cordea.recyclerviewlibrarycomparison.MainRepository
 import jp.cordea.recyclerviewlibrarycomparison.R
+import jp.cordea.recyclerviewlibrarycomparison.databinding.ActivityGraywaterBinding
+import org.intellij.lang.annotations.JdkConstants
+import javax.inject.Inject
 
 class GraywaterActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var repository: MainRepository
+
+    @Inject
+    lateinit var adapter: GraywaterAdapter
+
+    private var disposable: Disposable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_graywater)
+        val binding = DataBindingUtil.setContentView<ActivityGraywaterBinding>(this, R.layout.activity_graywater)
+        binding.recyclerView.adapter = adapter
+
+        disposable = repository
+            .getLanguages()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ langs ->
+                langs.forEach { adapter.add(GraywaterItemModel.from(it)) }
+            }, { })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }
