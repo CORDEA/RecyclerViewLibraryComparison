63c12388326602b234008a71f98b286250a0f49f
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-08 17:30:23 +0900

    add new activity

---
+++ app/src/main/java/jp/cordea/recyclerviewlibrarycomparison/bento/BentoActivity.kt
@@ -0,0 +1,13 @@
+package jp.cordea.recyclerviewlibrarycomparison.bento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.cordea.recyclerviewlibrarycomparison.R
+
+class BentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bento)
+    }
+}
